8aa062c7de661b43f11c8accf141fdeee77ae121
J1J2J <55465872+J1J2J@users.noreply.github.com>
2020-01-09 20:04:27 -0700

    Corrected Initial Commit

--- Skystone/Primary Bot/Encoder_Methods_Stop.java
+++ Skystone/Primary Bot/Encoder_Methods_Stop.java
@@ -1,75 +1,74 @@
-// Methods 
+final int OFF = 0;   
+   // Method for encoder BACKWARD mpovement
+     public void backwardByEncoder(double speed, double distance){
+         robot.reset();
+         // while (((-bl.getCurrentPosition() < distance)) && (distance - -bl.getCurrentPosition)){
+         while (((-robot.bl.getCurrentPosition() > distance) && !isStopRequested())){
+         robot.fl.setPower(-speed);
+         robot.fr.setPower(-speed);
+         robot.bl.setPower(-speed);
+         robot.br.setPower(-speed);
+         // -speed was BACKWARD
+        
+         }
+         robot.fl.setPower(OFF);
+         robot.fr.setPower(OFF);
+         robot.bl.setPower(OFF);
+         robot.br.setPower(OFF);
+     }    
+     // Methods 
     public void forwardByEncoder(double speed, double distance){
         // while (((-bl.getCurrentPosition() < distance)) && (distance - -bl.getCurrentPosition)){
-        reset();
-        while ((-bl.getCurrentPosition() < distance) && !isStopRequested()){
+        robot.reset();
+        while ((-robot.bl.getCurrentPosition() < distance) && !isStopRequested()){
         //  fl.setPower(FORWARD);
         //  fr.setPower(FORWARD);
         //  bl.setPower(FORWARD);
         //  br.setPower(FORWARD);
-            fl.setPower(speed);
-            fr.setPower(speed);
-            bl.setPower(speed);
-            br.setPower(speed);
+            robot.fl.setPower(speed);
+            robot.fr.setPower(speed);
+            robot.bl.setPower(speed);
+            robot.br.setPower(speed);
         }
-        fl.setPower(OFF);
-        fr.setPower(OFF);
-        bl.setPower(OFF);
-        br.setPower(OFF);
+         robot.fl.setPower(OFF);
+         robot.fr.setPower(OFF);
+         robot.bl.setPower(OFF);
+         robot.br.setPower(OFF);
     }  
-    
-    // Method for encoder BACKWARD mpovement
-     public void backwardByEncoder(double speed, double distance){
-         reset();
-         // while (((-bl.getCurrentPosition() < distance)) && (distance - -bl.getCurrentPosition) && !isStopRequested()){
-         while (((-bl.getCurrentPosition() > distance))){
-         fl.setPower(-speed);
-         fr.setPower(-speed);
-         bl.setPower(-speed);
-         br.setPower(-speed);
-         // -speed was BACKWARD
-        
-         }
-         fl.setPower(OFF);
-         fr.setPower(OFF);
-         bl.setPower(OFF);
-         br.setPower(OFF);
-     }    
-    
     
     // Method for encoder RIGHT movement
      public void rightByEncoder(double speed, double distance){
-        reset();
-        while (((fr.getCurrentPosition() < distance)) && !isStopRequested()){
+        robot.reset();
+        while (((robot.fr.getCurrentPosition() < distance) && !isStopRequested())){
         // Note: +speed was FORWARD, -speed was BACK
-        fl.setPower(speed);
-        fr.setPower(-speed);
-        bl.setPower(-speed);
-        br.setPower(speed);
+        robot.fl.setPower(speed);
+        robot.fr.setPower(-speed);
+        robot.bl.setPower(-speed);
+        robot.br.setPower(speed);
         }
-         fl.setPower(OFF);
-         fr.setPower(OFF);
-         bl.setPower(OFF);
-         br.setPower(OFF);
+         robot.fl.setPower(OFF);
+         robot.fr.setPower(OFF);
+         robot.bl.setPower(OFF);
+         robot.br.setPower(OFF);
      }    
     
     
     // Method for encoder LEFT mpovement
      public void leftByEncoder(double speed, double distance){
-         reset();
-         while (((fr.getCurrentPosition() > distance)) && !isStopRequested()){
+         robot.reset();
+         while (((robot.fr.getCurrentPosition() > distance)) && !isStopRequested()){
             // Note: +speed was FORWARD, -speed was BACK
-            fl.setPower(-speed);
-            fr.setPower(speed);
-            bl.setPower(speed);
-            br.setPower(-speed);
+            robot.fl.setPower(-speed);
+            robot.fr.setPower(speed);
+            robot.bl.setPower(speed);
+            robot.br.setPower(-speed);
         // fl.setPower(-Math.abs(speed));
         // fr.setPower(Math.abs(speed));
         // bl.setPower(Math.abs(speed));
         // br.setPower(-Math.abs(speed)); 
          }
-         fl.setPower(OFF);
-         fr.setPower(OFF);
-         bl.setPower(OFF);
-         br.setPower(OFF);
+         robot.fl.setPower(OFF);
+         robot.fr.setPower(OFF);
+         robot.bl.setPower(OFF);
+         robot.br.setPower(OFF);
      }
